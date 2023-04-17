c2ba6977720b73e55af8c1bc2a88ddf679d73fbb
shahriarmehedi <sau.shahriar@gmail.com>
2023-04-18 03:37:14 +0600

    init

---
+++ components/shared/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='h-[80px] bg-white shadow-lg'>
+            <div className="flex justify-between">
+                <div>
+                    <picture>
+                        <img src="/img/logoipsum-262 (4) 1.svg" alt="" />
+                    </picture>
+                </div>
+                <div>
+                    other
+                </div>
+            </div>
+        </div>
+    )
+}
